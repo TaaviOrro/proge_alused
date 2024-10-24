efce702af5b20ed2b666ac88346b9160ccbad7c6
Taavi <taavi.orro@voco.ee>
2024-10-24 12:26:27 +0300

    ylesanne 3.2

---
+++ yl3.2.js
@@ -0,0 +1,20 @@
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question('Sisestage ringide arv: ', ringideArv=> {
+    let porgandid = 0
+    for(let ring = 1; ring <= ringideArv; ring++){ 
+        if(ring % 2 == 0){
+            porgandid = porgandid + ring
+            console.log(ring, porgandid)
+            
+        }
+        
+    }
+    console.log(`Porgandite koguarv on ${porgandid}`)
+    rl.close()
+})
